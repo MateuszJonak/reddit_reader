f8e73633499dbef094539b764e645f6c672bf2af
m.jonak@dook.pl <syncmaster>
2014-07-08 19:05:37 +0200

    first look for getting articles titles by subreddits

--- app/scripts/controllers/main.js
+++ app/scripts/controllers/main.js
@@ -14,14 +14,20 @@ angular.module('redditReaderApp')
           'AngularJS',
           'Karma'
         ];
-        $http({method: 'GET', url: 'http://www.reddit.com/r/jquery/new.json?sort=new'}).
-            success(function(data, status, headers, config) {
-                console.log(data);
-                // this callback will be called asynchronously
-                // when the response is available
-            }).
-            error(function(data, status, headers, config) {
-                // called asynchronously if an error occurs
-                // or server returns response with an error status.
-            });
+
+        $scope.display = function(){
+            $scope.articles = [];
+            $http({method: 'GET', url: 'http://www.reddit.com/r/' + $scope.subreddit + '/new.json?sort=new'}).
+                success(function(data, status, headers, config) {
+                    var articles = data.data.children;
+                    for (var i=0; i<articles.length; i++ ){
+                        $scope.articles.push(articles[i].data);
+                    }
+                }).
+                error(function(data, status, headers, config) {
+                    // called asynchronously if an error occurs
+                    // or server returns response with an error status.
+                });
+        }
+
     });
